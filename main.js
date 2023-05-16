59bc72c5ca22f85a3b7906320e6f152c06a74012
Dave89dc <127298096+Dave89dc@users.noreply.github.com>
2023-05-16 08:52:25 +0200

    final alpha

--- main.js
+++ main.js
@@ -36,3 +36,83 @@ function displayListOfUsers(){
 
 }
 
+
+//<button id="save-btn" onclick=" createUser()">SAVE</button>
+
+function createUser(){
+    const name=document.getElementById("name-input").value;
+    const surname=document.getElementById("surname-input").value;
+    //const gender=document.getElementById("gender-input").checked; if checkbox, always value if text or radio
+    const gender=document.getElementById("gender-input").value;
+    const yob=document.getElementById("yob-input").value;
+    const username=document.getElementById("user-name-input").value;
+    const password=document.getElementById("password-input").value;
+    const password_check=document.getElementById("confirm-password-input").value;
+    const email=document.getElementById("email-input").value;
+    const phonenumber=document.getElementById("phone-input").value;
+    const _newsletter=document.getElementById("news-letter-input").checked;
+    //ADD A HIDDEN DIV!! <div id="error-display"></div> after REGISTRATION BOX in registration page, css display hidden
+    const displayError=document.getElementById("error-display");
+    displayError.style.display = "none";
+   //  console.log(name);
+   //  console.log(surname);
+   //  console.log(gender);
+   //  console.log(yob);
+   //  console.log(username);
+   //  console.log(password);
+   //  console.log(password_check);
+   //  console.log(email);
+   //  console.log(phonenumber);
+   //  console.log(_newsletter);
+   
+    console.log('test');
+    displayError.innerHTML=``;
+       let errorStr='';
+    if(password !== password_check){
+       errorStr+=`${errorStr === ''?'Caro utente,':''} la password non è confermata correttamente. `;
+   }
+    if(password.length<8){
+       errorStr+=`${errorStr === ''?'Caro utente, l':'L'}a password deve essere almeno lunga 8 caratteri `;
+    }
+    if(name.length<3 || surname.length<3 || username.length<3 ){
+       errorStr+=`${errorStr === ''?'Caro utente, l':'L'}a informiamo che il nome, cognome e username richiedono un minimo di 3 caratteri per essere validi `;
+    }
+    const emailRegex = new RegExp('^(.+)@(.+)$')
+    if(!emailRegex.test(email)){
+       errorStr+=`${errorStr === '' ? 'Caro utente, l' : 'L'}a mail è incorretta `
+    }
+   
+    if(errorStr===''){
+       const newUser = new User(username, name, surname, email, password, yob, _newsletter, gender, phonenumber);
+       newListOfUsers.addUser(newUser);
+       console.log(newUser);
+       console.log(newListOfUsers);
+       displayListOfUsers();
+   
+       name.value= '';
+       surname.value= '';
+       gender.value= '';
+       yob.value= '';
+       username.value= '';
+       password.value= '';
+       password_check.value= '';
+       email.value= '';
+       phonenumber.value= '';
+       _newsletter.checked= false;
+    }
+     else
+     {
+       errorStr += 'Riprova inserendo i dati corretti, grazie!';
+       displayError.style.display = 'block';
+       displayError.innerHTML = errorStr;
+     }
+   
+   
+   }
+   
+   // /* * AGGIUNTE CHERUBINI **/
+   // #error-display{
+   //     display: none;
+   //     text-shadow: 2px 2px 12px rgba(248, 248, 255, 0.8);
+   //     color: rgb(197, 0, 0);
+   // }  E DIV CON ID displayError
