3547f450a694083c859f6f972f248cd76c220421
Dave89dc <127298096+Dave89dc@users.noreply.github.com>
2023-05-17 11:34:24 +0200

    Update users-service.js

--- service/users-service.js
+++ service/users-service.js
@@ -1,29 +1,29 @@
 // fetch finale:
 
-class UsersService{
-
-    static BASE_URL = 'https://my-first-api-devops-nrj0fgwn8-sunshinemocha.vercel.app/users';
-
-    static getUsersServive(){
-        return fetch(this.BASE_URL).then(resp => resp.json());
-    }
-
-    static postUser(user){ 
-        const jsonUser = JSON.stringify(user.toDbModel());
-        return fetch(this.BASE_URL,{method: 'POST', headers: {'content-type':'application/json'}, body: jsonUser});
-    }
-
-}
-
-// fetch di prova:
-
 // class UsersService{
 
-//     static BASE_URL = './mock-data/mock-users.json';
+//     static BASE_URL = 'https://my-first-api-devops-nrj0fgwn8-sunshinemocha.vercel.app/users';
 
 //     static getUsersServive(){
 //         return fetch(this.BASE_URL).then(resp => resp.json());
-//     };
+//     }
 
-//}
+//     static postUser(user){ 
+//         const jsonUser = JSON.stringify(user.toDbModel());
+//         return fetch(this.BASE_URL,{method: 'POST', headers: {'content-type':'application/json'}, body: jsonUser});
+//     }
+
+// }
+
+// fetch di prova:
+
+class UsersService{
+
+    static BASE_URL = './mock-data/mock-users.json';
+
+    static getUsersServive(){
+        return fetch(this.BASE_URL).then(resp => resp.json());
+    };
+
+}
 
